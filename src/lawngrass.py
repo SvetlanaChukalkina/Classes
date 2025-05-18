733a362afc30907fc5d8bb5e44579c25075c01ea
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-05-18 19:23:47 +0300

    small fixes

--- src/lawngrass.py
+++ src/lawngrass.py
@@ -6,7 +6,16 @@ from src.product import Product
 class LawnGrass(Product):
     """Класс для представления травы газонной, наследник класса Product"""
 
-    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str, color: str) -> None:
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str,
+    ) -> None:
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
